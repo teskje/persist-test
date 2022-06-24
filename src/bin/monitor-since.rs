00db87bc6a324bdb98762240f32f461c27382b73
Jan Teske <jteske@posteo.net>
2022-06-24 16:00:59 +0200

    add persist test utils

---
+++ src/bin/monitor-since.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::time::Duration;
+
+use mz_dataflow_types::sources::SourceData;
+use mz_persist_client::PersistLocation;
+use mz_repr::{Diff, Timestamp};
+use tokio::time;
+
+#[tokio::main]
+async fn main() {
+    let mut args = env::args().skip(1);
+    let consensus_uri = args.next().expect("missing consensus URI");
+    let blob_uri = args.next().expect("missing blob URI");
+    let shard_id = args
+        .next()
+        .expect("missing shard ID")
+        .parse()
+        .expect("invalid shard ID");
+
+    let location = PersistLocation {
+        blob_uri,
+        consensus_uri,
+    };
+    let client = location
+        .open()
+        .await
+        .expect("could not open persist client");
+
+    loop {
+        let probe = client
+            .open_reader::<SourceData, (), Timestamp, Diff>(shard_id)
+            .await
+            .expect("could not open persist shard");
+        let since = probe.since().elements()[0];
+        println!("{since}");
+        probe.expire().await;
+
+        time::sleep(Duration::from_secs(1)).await;
+    }
+}
---
+++ src/bin/tail.rs
@@ -0,0 +1,65 @@
+use std::env;
+
+use mz_dataflow_types::sources::SourceData;
+use mz_persist_client::read::ListenEvent;
+use mz_persist_client::PersistLocation;
+use mz_repr::{Diff, Timestamp};
+
+#[tokio::main]
+async fn main() {
+    let mut args = env::args().skip(1);
+    let consensus_uri = args.next().expect("missing consensus URI");
+    let blob_uri = args.next().expect("missing blob URI");
+    let shard_id = args
+        .next()
+        .expect("missing shard ID")
+        .parse()
+        .expect("invalid shard ID");
+
+    let location = PersistLocation {
+        blob_uri,
+        consensus_uri,
+    };
+    let mut reader = location
+        .open()
+        .await
+        .expect("could not open persist client")
+        .open_reader::<SourceData, (), Timestamp, Diff>(shard_id)
+        .await
+        .expect("could not open persist shard");
+
+    let as_of = reader.since();
+
+    let mut snapshot = reader
+        .snapshot(as_of.clone())
+        .await
+        .expect("could not serve requested as_of");
+
+    while let Some(updates) = snapshot.next().await {
+        for ((key, _), ts, diff) in updates {
+            let row = key.unwrap().0.unwrap();
+            println!("{row}, {ts}, {diff}");
+        }
+    }
+
+    let mut listen = reader
+        .listen(as_of.clone())
+        .await
+        .expect("cannot serve requested as_of");
+
+    loop {
+        for event in listen.next().await {
+            match event {
+                ListenEvent::Progress(upper) => {
+                    reader.downgrade_since(upper.clone()).await;
+                }
+                ListenEvent::Updates(updates) => {
+                    for ((key, _), ts, diff) in updates {
+                        let row = key.unwrap().0.unwrap();
+                        println!("{row}, {ts}, {diff}");
+                    }
+                }
+            }
+        }
+    }
+}
